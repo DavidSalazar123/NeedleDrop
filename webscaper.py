a1456656a5d44626f705a19ccee8192c88d4cd86
DavidSalazar123 <davidsalazar200@gmail.com>
2022-12-25 22:26:04 -0500

    Updated library and code

--- webscaper.py
+++ webscaper.py
@@ -1,89 +1,80 @@
-import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
-import requests
 import pandas as pd
 import time
-import math
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from pytube import YouTube
 import re
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 HOME = "https://www.youtube.com/user/theneedledrop/videos"
+GENRE_PATTERN = re.compile("[^\/](19|20)\d{2}\s\/.*")
 
 
 def scroll():
-    """
-    scrolls to the bottom of the page
-    code taken from https://stackoverflow.com/questions/20986631/how-can-i-scroll-a-web-page-using-selenium-webdriver-in-python
-    modified slightly for YouTube
-    """
-    SCROLL_PAUSE_TIME = 0.5
+    SCROLL_PAUSE_TIME = 1
+    last_height = driver.execute_script(
+        "return document.documentElement.scrollHeight")
 
-    last_height = driver.execute_script("return window.scrollY")
-
-    tries = 0
     while True:
-        down_height = last_height + 1000
-        driver.execute_script("window.scrollTo(0," + str(down_height) + ")")
-
+        driver.execute_script(
+            "window.scrollTo(0,document.documentElement.scrollHeight);")
         time.sleep(SCROLL_PAUSE_TIME)
-
-        new_height = driver.execute_script("return window.scrollY")
+        new_height = driver.execute_script(
+            "return document.documentElement.scrollHeight")
         if new_height == last_height:
-            tries += 1
-            time.sleep(SCROLL_PAUSE_TIME)
-            if tries == 50:
-                break
-        else:
-            tries = 0
+            time.sleep(3)
+            break
         last_height = new_height
 
 
-# Scrape for videos
-driver = webdriver.Chrome("C:\webdriver\chromedriver.exe")
+# Webdriver to scrape youtube video
+data = []
+
+# Options
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-notifications")
+options.add_argument("--disable-Advertisement")
+options.add_argument("--disable-popup-blocking")
+options.add_argument("start-maximized")
+s=Service(r"C:\webdriver\chromedriver.exe")
+driver= webdriver.Chrome(service=s,options=options)
 driver.get(HOME)
 
+time.sleep(3)
 scroll()
-element_titles = driver.find_elements_by_id("video-title")
 
+try:
+    for e in WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div#details'))):
+        vurl = e.find_element(
+            By.CSS_SELECTOR, 'a#video-title-link').get_attribute('href')
+        data.append({
+            'video_url': vurl,
+        })
+except:
+    pass
+df = pd.DataFrame(data).drop_duplicates()
+df.to_csv('./ytlinks.csv')
+
+print("Starting to scrape Youtube videos...")
 # Creation of the data
-row = []
-GENRE_PATTERN = re.compile("[^\/](19|20)\d{2}\s\/.*")
-PATTERN = re.compile('(?<=shortDescription":").*(?=","isCrawlable)')
-for element in element_titles:
-    title = element.text
-
-# Not Good and Classics are included within this data
-# Change code to get the description of video instead
+# WARNING: Takes long time
+musicdataDF = pd.DataFrame(columns=["Title", "Description", "VidLength", "Views", "PublishDate","Genre"])
+df = pd.read_csv('./ytlinks.csv')
+link_list = df['video_url']
+KEYWORDS = ['track review', 'yunoreview', 'weekly track']
+for link in link_list:
     try:
-        if "review" in title.lower() and "track review" not in title.lower() and "yunoreview" not in title.lower() and "weekly track" not in title.lower():
-
-            # Get Description of video
-            soup = BeautifulSoup(requests.get(
-                element.get_attribute("href")).content)
-            description = PATTERN.findall(
-                str(soup))[0].replace('\\n', '\n').lower()
-
-            # Get Score of the video
-            # Prevents word classification to not be included (Classic, meh, Not Good)
-            score = re.search(
-                "[^201]\n\d{0,2}\/10", description).group().strip("\n").split("/")[0]
-
-            # Get the Genre
-            genre = GENRE_PATTERN.search(description).group()
-
-            # Get the year the album dropped
-            album_date = GENRE_PATTERN.search(
-                description).group().split("/")[0].strip()
-
-            # Date of when the video was published
-            video_date = soup.find("meta", itemprop="datePublished")['content']
-
-            row.append([title, genre, album_date, video_date, score])
-            print([title, genre, album_date, video_date, score])
+        video = YouTube(link)
+        title = video.title.lower()
+        if title not in KEYWORDS and "review" in title:
+            description = video.description
+            row = {"Title": title, "Description": description, "VidLength": video.length, "Views": video.views,"PublishDate": video.publish_date, "Genre": GENRE_PATTERN.search(description).group()}
+            musicdataDF = musicdataDF.append(row, ignore_index=True)
     except Exception:
         pass
 
-music_data = pd.DataFrame(
-    row, columns=["Title", "Genre", "Album_Date", "Video_Date", "Score"])
-
-# Save the music data to csv
-music_data.to_csv("..\NeedleDrop\music_data.csv")
+musicdataDF.to_csv('./reviews_videos.csv')
